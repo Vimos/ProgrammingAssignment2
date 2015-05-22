5d586d92f91e994ba8816a0bc92d2a800504e817
Vimos Tan <vimostan@gmail.com>
2015-05-22 16:31:02 +0800

    commit cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## i: cached inverse of matrix 
+## getinv: get cached inverse
+## setinv: set cached inverse
+## set: set matrix to obj
+## get: get matrix from obj
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+            x <<- y
+            i <<- NULL
+    }
+    get <- function() x
+    setinv<- function(inv) i <<- inv
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
+## if inverse exists in cache, get cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+        message("getting cached inverse")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    i
 }
